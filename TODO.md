8f6556e5d4a049395081987db58f3f1920456665
maxjgarcia <garcia.maximiliano.jesus@gmail.com>
2024-12-28 07:27:26 -0300

    working on upcoming matches

---
+++ TODO.md
@@ -0,0 +1,33 @@
+## Pagina ID
+
+[x] cambiar la manera de generar los id
+
+## GALERIA
+
+[x] imágenes estáticas en component
+
+[x] refactorizar lógica de galería y estilos
+
+[x] server:defer en galería y output en config
+
+[x] eliminar segunda galeria de test
+
+[x] images alts
+
+## Footer
+
+[] position
+
+[x] ajustar iconos
+
+[] cargar redes e info
+
+## [id]
+
+[] redisenar el display
+
+## MAIN
+
+[] agregar para fade in de content
+
+[] agregar 'upcoming matches'
---
+++ tailwind.config.mjs
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
+import typography from '@tailwindcss/typography'
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'primary-d-blue': '#174897',
+				'primary-m-blue': '#2886BB',
+				'primary-l-blue': '#39C3DE',
+			},
+		},
+	},
+	plugins: [typography, animations, function ({ addUtilities, theme, e }) {
+		addUtilities(
+			{
+				'.gradient-text': {
+					backgroundImage: 'linear-gradient(to left, #174897, #39C3DE)',
+					'-webkit-background-clip': 'text',
+					color: 'transparent',
+				},
+				'.btn-bfg': {
+					background: 'linear-gradient(to top left, #174897, #39C3DE)',
+					border: 'solid',
+					borderWidth: '0.1rem',
+					borderColor: 'rgb(255 255 255 / 0.7)',
+					padding: '0.2rem 1rem',
+					color: 'white',
+
+
+					transition: 'all 0.3s ease',
+				},
+			},
+			['responsive', 'hover']
+		);
+	},],
+}
