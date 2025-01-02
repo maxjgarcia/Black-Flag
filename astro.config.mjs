d4058366bee233c6ec01ae478fd855dd2f2aa6b2
maxjgarcia <garcia.maximiliano.jesus@gmail.com>
2025-01-02 08:15:56 -0300

    countdown and results added

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,14 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-  // output: 'server',
-  integrations: [tailwind(), react(), icon()]
+  output: 'server',
+  integrations: [tailwind(), react(), icon()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
